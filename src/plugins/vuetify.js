fc17a2e3509602068590557db3edf39afd668047
FlyingSky-CN <me@fsky7.com>
2022-02-08 08:55:30 +0800

    require MDI into local and remove Google Fonts.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,14 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import colors from 'vuetify/lib/util/colors';
+import '@mdi/font/css/materialdesignicons.css';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    icons: {
+        iconfont: 'mdi',
+    },
     theme: {
         themes: {
             light: {
