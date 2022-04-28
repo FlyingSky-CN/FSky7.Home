8f87d43f9952631bf3fea62d29610c68d1f0c55b
FlyingSky-CN <me@fsky7.com>
2022-04-29 07:18:54 +0800

    social media: Weibo -> Mastodon

--- src/components/Site.vue
+++ src/components/Site.vue
@@ -1,7 +1,7 @@
 <template>
     <v-row class="mx-2 mt-n2" no-gutters style="opacity: .9;">
         <v-col cols="6" md="3" v-for="(item, i) in data" :key="i">
-            <v-card elevation="0" class="mx-2 mb-4" :color="item.color" dark :href="item.href" :title="item.value">
+            <v-card elevation="0" class="mx-2 mb-4" :rel="item.key=='Mastodon' ? 'me' : ''" :color="item.color" dark :href="item.href" :title="item.value">
                 <v-card-title>
                     <span class="title font-weight-light">{{item.key}}</span>
                 </v-card-title>
